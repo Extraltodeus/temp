6bd55c0f66a0459048e7f5c4bf34fd869872855b
Extraltodeus <extraltodeus@gmail.com>
2022-10-21 21:40:20 +0200

    Update devices2.py

--- devices2.py
+++ devices2.py
@@ -13,11 +13,11 @@ cpu = torch.device("cpu")
 def get_optimal_device():
     if torch.cuda.is_available():
         from modules import shared
-        print(shared.cmd_opts.device_id)
-        if shared.cmd_opts.device_id is not None:
-            return torch.device(f"cuda:{shared.cmd_opts.device_id}")
-        else:
-            return torch.device("cuda")
+        print(shared)
+#         if shared.cmd_opts.device_id is not None:
+#             return torch.device(f"cuda:{shared.cmd_opts.device_id}")
+#         else:
+        return torch.device("cuda")
 
     if has_mps:
         return torch.device("mps")
