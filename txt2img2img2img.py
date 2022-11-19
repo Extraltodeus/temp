227ca0a4547cbb96193a0bb9c83c3ea095e196f2
Extraltodeus <extraltodeus@gmail.com>
2022-11-19 14:28:14 +0100

    Update txt2img2img2img.py

--- txt2img2img2img.py
+++ txt2img2img2img.py
@@ -12,7 +12,7 @@ class Script(scripts.Script):
     def ui(self, is_img2img):
         if is_img2img: return
         img2img_samplers_names = [s.name for s in sd_samplers.samplers_for_img2img]
-        t2iii_reprocess = gr.Slider(minimum=0, maximum=10, step=1, label='Number of img2img', value=3)
+        t2iii_reprocess = gr.Slider(minimum=1, maximum=10, step=1, label='Number of img2img', value=3)
         t2iii_steps = gr.Slider(minimum=1, maximum=120, step=1, label='img2img steps', value=30)
         t2iii_cfg_scale = gr.Slider(minimum=1, maximum=30, step=0.1, label='img2img cfg scale', value=12)
         t2iii_seed_shift = gr.Slider(minimum=0, maximum=1000000, step=1, label='img2img new seed+', value=1000)
@@ -20,14 +20,28 @@ class Script(scripts.Script):
         t2iii_upscale_factor = gr.Slider(minimum=1, maximum=4, step=0.1, label='Stretch before save (factor, 1=disabled)', value=2)
         t2iii_only_last = gr.Checkbox(label='Only save the last img2img', value=True)
         t2iii_sampler = gr.Dropdown(label="Sampler", choices=img2img_samplers_names, value="DDIM")
-        return [t2iii_reprocess,t2iii_steps,t2iii_cfg_scale,t2iii_seed_shift,t2iii_denoising_strength,t2iii_upscale_factor,t2iii_only_last,t2iii_sampler]
+        t2iii_upscale_x = gr.Slider(minimum=64, maximum=2048, step=64, label='img2img width (64 = no rescale)', value=64)
+        t2iii_upscale_y = gr.Slider(minimum=64, maximum=2048, step=64, label='img2img height (64 = no rescale)', value=64)
+        return [t2iii_reprocess,t2iii_steps,t2iii_cfg_scale,t2iii_seed_shift,t2iii_denoising_strength,t2iii_upscale_factor,t2iii_only_last,t2iii_sampler,t2iii_upscale_x,t2iii_upscale_y]
 
-    def run(self,p,t2iii_reprocess,t2iii_steps,t2iii_cfg_scale,t2iii_seed_shift,t2iii_denoising_strength,t2iii_upscale_factor,t2iii_only_last,t2iii_sampler):
+    def run(self,p,t2iii_reprocess,t2iii_steps,t2iii_cfg_scale,t2iii_seed_shift,t2iii_denoising_strength,t2iii_upscale_factor,t2iii_only_last,t2iii_sampler,t2iii_upscale_x,t2iii_upscale_y):
+        img2img_samplers_names = [s.name for s in sd_samplers.samplers_for_img2img]
+        img2img_sampler_index = [i for i in range(len(img2img_samplers_names)) if img2img_samplers_names[i] == t2iii_sampler][0]
+
         def simple_upscale(img, factor):
             w, h = img.size
             w = int(w * factor)
             h = int(h * factor)
             return img.resize((w, h), Image.LANCZOS)
+
+        if t2iii_upscale_x > 64:
+            upscale_x = t2iii_upscale_x
+        else:
+            upscale_x = p.width
+        if t2iii_upscale_y > 64:
+            upscale_y = t2iii_upscale_y
+        else:
+            upscale_y = p.height
 
         n_iter=p.n_iter
         for j in range(n_iter):
@@ -62,14 +76,13 @@ class Script(scripts.Script):
                     seed_resize_from_h=p.seed_resize_from_h,
                     seed_resize_from_w=p.seed_resize_from_w,
                     #seed_enable_extras=p.seed_enable_extras,
-                    # sampler_index=p.sampler_index,
-                    sampler=t2iii_sampler,
+                    sampler_index=img2img_sampler_index,
                     batch_size=p.batch_size,
                     n_iter=p.n_iter,
                     steps=t2iii_steps,
                     cfg_scale=t2iii_cfg_scale,
-                    width=p.width,
-                    height=p.height,
+                    width=upscale_x,
+                    height=upscale_y,
                     restore_faces=p.restore_faces,
                     tiling=p.tiling,
                     do_not_save_samples=True,
