110b1e5f114bf5009a4a39f9acd1e99e5ec32d6b
Extraltodeus <extraltodeus@gmail.com>
2022-11-20 22:05:13 +0100

    Update lanczos_stretch.py

--- lanczos_stretch.py
+++ lanczos_stretch.py
@@ -16,20 +16,23 @@ from modules.shared import opts, cmd_opts, state
 
 class Script(scripts.Script):
     alwayson = True
-    
+
     def __init__(self):
         on_before_image_saved(self.bis)
-    
+
+    def process(self, p, simple_upscale_factor):
+        p.simple_upscale_factor = simple_upscale_factor
+
     def title(self):
         return "Lanczos simple upscale"
-      
+
     def show(self, is_img2img):
         return scripts.AlwaysVisible
 
     def ui(self, is_img2img):
         simple_upscale_factor = gr.Slider(minimum=1, maximum=4, step=0.1, label='Upscale factor', value=2)
         return [simple_upscale_factor]
-          
+
     def bis(self, params):
         if params.p.simple_upscale_factor > 1:
             w, h = params.image.size
@@ -37,4 +40,3 @@ class Script(scripts.Script):
             h = int(h * params.p.simple_upscale_factor)
             image = params.image.resize((w, h), Image.Resampling.LANCZOS)
             params.image = image
-#             images.save_image(image, ImageSaveParams.p.outpath_samples, ImageSaveParams.filename, ImageSaveParams.p.seed, ImageSaveParams.p.prompt, opts.samples_format, info=ImageSaveParams.pnginfo, p=ImageSaveParams.p)
