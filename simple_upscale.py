5afd4e65a86a3a3320d3ef0e82ea31908b504e51
Extraltodeus <extraltodeus@gmail.com>
2022-11-20 21:56:58 +0100

    Update simple_upscale.py

--- simple_upscale.py
+++ simple_upscale.py
@@ -6,6 +6,7 @@ import traceback
 import random
 
 import modules.scripts as scripts
+from   modules.script_callbacks import on_before_image_saved
 import modules.images as images
 import gradio as gr
 
@@ -15,20 +16,27 @@ from modules.shared import opts, cmd_opts, state
 
 class Script(scripts.Script):
     alwayson = True
+
+    def __init__(self):
+        on_before_image_saved(self.bis)
+
+    def process(self, p, simple_upscale_factor):
+        p.simple_upscale_factor = simple_upscale_factor
+
     def title(self):
         return "Lanczos simple upscale"
-      
+
     def show(self, is_img2img):
         return scripts.AlwaysVisible
 
     def ui(self, is_img2img):
         simple_upscale_factor = gr.Slider(minimum=1, maximum=4, step=0.1, label='Upscale factor', value=2)
         return [simple_upscale_factor]
-      
-    def batch_postprocess(self, p, image, simple_upscale_factor, **batch_properties):
-        if simple_upscale_factor > 1:
-            w, h = image.size
-            w = int(w * simple_upscale_factor)
-            h = int(h * simple_upscale_factor)
-            image = image.resize((w, h), Image.Resampling.LANCZOS)
-            images.save_image(image, p.outpath_samples, "", batch_properties['seed'], batch_properties['prompt'], opts.samples_format, info=batch_properties['info'], p=p)
+
+    def bis(self, params):
+        if params.p.simple_upscale_factor > 1:
+            w, h = params.image.size
+            w = int(w * params.p.simple_upscale_factor)
+            h = int(h * params.p.simple_upscale_factor)
+            image = params.image.resize((w, h), Image.Resampling.LANCZOS)
+            params.image = image
