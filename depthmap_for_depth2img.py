54a8b5900ce39fb8d08b401545e75e7404a18820
Extraltodeus <extraltodeus@gmail.com>
2022-11-25 04:17:42 +0100

    Update depthmap_for_depth2img.py

--- depthmap_for_depth2img.py
+++ depthmap_for_depth2img.py
@@ -27,25 +27,23 @@ class SimpleDepthMapGenerator(object):
                     for block in response.iter_content(4096):
                         fout.write(block)
 
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
             # model path and name
             model_dir = "./models/midas"
             # create path to model if not present
             os.makedirs(model_dir, exist_ok=True)
             print("Loading midas model weights ..")
-            model_path = f"{model_dir}/dpt_hybrid-midas-501f0c75.pt"
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            model_path = f"{model_dir}/midas_v21-f6b98070.pt"
             print(model_path)
             if not os.path.exists(model_path):
-                download_file(model_path,"https://github.com/intel-isl/DPT/releases/download/1_0/dpt_hybrid-midas-501f0c75.pt")
-            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-            print("device: %s" % device)
-            model = DPTDepthModel(
-                path=model_path,
-                backbone="vitb_rn50_384",
-                non_negative=True,
-            )
+              download_file(model_path,"https://github.com/AlexeyAB/MiDaS/releases/download/midas_dpt/midas_v21-f6b98070.pt")
+            model = MidasNet(model_path, non_negative=True)
             net_w, net_h = 384, 384
-            resize_mode="minimal"
-            normalization = NormalizeImage(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+            resize_mode="upper_bound"
+            normalization = NormalizeImage(
+              mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+            )
             # init transform
             transform = Compose(
                 [
@@ -117,7 +115,7 @@ class SimpleDepthMapGenerator(object):
             img_output2[:,:,2] = img_output / 256.0
             img = Image.fromarray(img_output2)
             return img
-        except Exception as e:
-            print(e)
+        except Exception:
+            raise
         finally:
             del model
