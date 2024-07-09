051e22c8bcec22f9fc2a4fb108f5e9f67ab5fe9b
Extraltodeus <extraltodeus@gmail.com>
2024-07-09 08:58:14 +0200

    Add files via upload

--- perp_neg_pre_cfg/__init__.py
+++ perp_neg_pre_cfg/__init__.py
@@ -3,6 +3,7 @@ from .nodes import *
 NODE_CLASS_MAPPINGS = {
     "Pre CFG perp-neg": pre_cfg_perp_neg,
     "Pre CFG sharpening": condDiffSharpeningNode,
+    "Pre CFG consensus sharpening": condConsensusSharpeningNode,
     "Pre CFG exponentiation": condExpNode,
     "Automatic CFG simplified": automatic_cfg_simplified
 }
--- perp_neg_pre_cfg/nodes.py
+++ perp_neg_pre_cfg/nodes.py
@@ -195,3 +195,111 @@ class automatic_cfg_simplified:
         m = model.clone()
         m.set_model_sampler_cfg_function(automatic_cfg)
         return (m, )
+
+@torch.no_grad()
+def euclidean_weights(tensors,exponent=2,proximity_exponent=1,min_score_into_zeros=0):
+    divider = tensors.shape[0]
+    if exponent == 0:
+        exponent = 6.55
+    device = tensors.device
+    distance_weights = torch.zeros_like(tensors).to(device = device)
+
+    for i in range(len(tensors)):
+        for j in range(len(tensors)):
+            if i == j: continue
+            current_distance = (tensors[i] - tensors[j]).abs() / divider
+            if proximity_exponent > 1:
+                current_distance = current_distance ** proximity_exponent
+            distance_weights[i] += current_distance
+
+
+    min_stack, _ = torch.min(distance_weights, dim=0)
+    max_stack, _ = torch.max(distance_weights, dim=0)
+    max_stack    = torch.where(max_stack == 0, torch.tensor(1), max_stack)
+    sum_of_weights = torch.zeros_like(tensors[0]).to(device = device)
+
+    max_stack -= min_stack
+
+    for i in range(len(tensors)):
+        distance_weights[i] -= min_stack
+        distance_weights[i] /= max_stack
+        distance_weights[i]  = 1 - distance_weights[i]
+        distance_weights[i]  = torch.clamp(distance_weights[i], min=0)
+        if min_score_into_zeros > 0:
+            distance_weights[i]  = torch.where(distance_weights[i] < min_score_into_zeros, torch.zeros_like(distance_weights[i]), distance_weights[i])
+        
+        if exponent > 1:
+            distance_weights[i] = distance_weights[i] ** exponent
+
+        sum_of_weights += distance_weights[i]
+    
+    mean_score = (sum_of_weights.mean() / divider) ** exponent
+
+    sum_of_weights = torch.where(sum_of_weights == 0, torch.zeros_like(sum_of_weights) + 1 / divider, sum_of_weights)
+    result = torch.zeros_like(tensors[0]).to(device = device)
+
+    for i in range(len(tensors)):
+        distance_weights[i] /= sum_of_weights
+        distance_weights[i]  = torch.where(torch.isnan(distance_weights[i]) | torch.isinf(distance_weights[i]), torch.zeros_like(distance_weights[i]), distance_weights[i])
+        result = result + tensors[i] * distance_weights[i]
+
+    return result, mean_score
+
+class condConsensusSharpeningNode:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+                                "model": ("MODEL",),
+                                "scale": ("FLOAT",   {"default": 0.75, "min": 0.0, "max": 10.0, "step": 1/20, "round": 1/100}),
+                              }
+                              }
+    RETURN_TYPES = ("MODEL",)
+    FUNCTION = "patch"
+
+    CATEGORY = "model_patches"
+
+    def patch(self, model, scale):
+        model_sampling = model.model.model_sampling
+        sigma_max = model_sampling.sigma(model_sampling.timestep(model_sampling.sigma_max)).item()
+        prev_conds   = []
+        prev_unconds = []
+
+        @torch.no_grad()
+        def sharpen_conds_pre_cfg(args):
+            nonlocal prev_conds, prev_unconds
+            conds_out = args["conds_out"]
+            uncond = torch.any(conds_out[1])
+            
+            sigma  = args["sigma"][0].item()
+
+            if sigma <= 1:
+                return conds_out
+
+            first_step = sigma > (sigma_max - 1)
+            if first_step:
+                prev_conds   = []
+                prev_unconds = []
+
+            prev_conds.append(conds_out[0] / conds_out[0].norm())
+            if uncond:
+                prev_unconds.append(conds_out[1] / conds_out[1].norm())
+
+            if len(prev_conds) > 3:
+                consensus_cond, mean_score_cond = euclidean_weights(torch.stack(prev_conds))
+                consensus_cond = consensus_cond * conds_out[0].norm()
+            if len(prev_unconds) > 3 and uncond:
+                consensus_uncond, mean_score_uncond = euclidean_weights(torch.stack(prev_unconds))
+                consensus_uncond = consensus_uncond * conds_out[1].norm()
+
+            for b in range(len(conds_out[0])):
+                for c in range(len(conds_out[0][b])):
+                        if len(prev_conds) > 3:
+                            conds_out[0][b][c] = normalize_adjust(conds_out[0][b][c], consensus_cond[b][c],  mean_score_cond * scale)
+                        if len(prev_unconds) > 3 and uncond:
+                            conds_out[1][b][c] = normalize_adjust(conds_out[1][b][c], consensus_uncond[b][c], mean_score_uncond * scale)
+
+            return conds_out
+
+        m = model.clone()
+        m.set_model_sampler_pre_cfg_function(sharpen_conds_pre_cfg)
+        return (m, )
